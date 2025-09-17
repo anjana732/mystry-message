a8f413dd98a390b8f9be3483aa6443bcd18d6022
anjana732 <anjanasingh1257@gmail.com>
2025-09-17 23:09:38 +0530

    add email verification

--- src/helpers/verificationEmail.ts
+++ src/helpers/verificationEmail.ts
@@ -1,1 +1,13 @@
 import {resend} from '@/lib/resend'
+import { ApiResponse } from '@/types/ApiResponse'
+import VerificationEmail from '../../Email/verificationEmail'
+
+export async function verificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse> {
+    try{
+        
+    }
+}
